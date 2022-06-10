0fa2cb9742b93fb33be77c9c9a1ee55a480bb126
John Hayes <hayesjohn54@yahoo.com>
2022-06-10 15:52:46 -0400

    commit tree abc0bcfb32e1b1a4864785648935854499793745

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+function findMatching(array, string){
+    const newArray = []
+    for(const user of array){
+        if(string == user){
+            newArray.push(user)
+        } else if (string == user){
+            newArray.push(user.toLowerCase)
+        }
+        
+    } 
+    return newArray;
+}
+
+
+
+function fuzzyMatch(array, string){
+    const newArray = []
+    for(const char of array){
+        if(string === char){
+            newArray.push(string)
+        }
+    }
+    return newArray;
+}
+
+function matchName(array, string){
+    const newArray = []
+    for(const item of array){
+        if(string === item.name){
+            newArray.push(string)
+        }
+    }
+    return newArray;
+}
